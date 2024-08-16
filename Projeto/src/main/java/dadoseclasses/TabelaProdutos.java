38c0424282c021c6b88e7c8a8e4b4fbe0585b11f
teste <88006072+izaiasbatis@users.noreply.github.com>
2024-08-15 21:11:26 -0300

    15-08

---
+++ Projeto/src/main/java/dadoseclasses/TabelaProdutos.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dadoseclasses;
+
+import java.util.ArrayList;
+import javax.swing.AbstractAction;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author Izaias
+ */
+public class TabelaProdutos  extends AbstractTableModel{
+    private ArrayList<CadastroProduto> TabelaProduto; 
+    
+
+    @Override
+    public int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int getColumnCount() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
